d39092ab024cae4a6598b9ce10637a8e7639adcc
gatmeister-c <alansebastian.soszynski@gmail.com>
2025-12-08 20:43:59 +0100

    Update sex-m.lua

--- sex-m.lua
+++ sex-m.lua
@@ -135,6 +135,18 @@ local function claimAllowance()
     end
 end
 
+local returnButton = playerGui:
+                        WaitForChild("CoreGUI"):
+                        WaitForChild("ReturnFrame"):
+                        WaitForChild("ReturnButton"):
+                        WaitForChild("TextButton")
+
+local function toMainMenu()
+    for i, connection in getconnections(returnButton.MouseButton1Down) do
+        connection:Fire()
+    end
+end
+
 local function focusCameraOnATM(atmMainPart)
 	local lookFrom = atmMainPart.Position + Vector3.new(0, 3, 5)
 	local lookTo = atmMainPart.Position
@@ -289,6 +301,7 @@ local function main()
     local loopCount = 0
     
     while task.wait(1) do
+        toMainMenu()
         if allowanceReady and not looping then
             fpscap = 60
             local newAtm = nil
